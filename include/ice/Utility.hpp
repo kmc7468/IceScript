976c1e77f7c56dbcb94b77f31ed3db5f1513ed3a
kmc7468 <kmc7468@naver.com>
2019-08-01 19:49:20 +0900

    Format 함수 추가

---
+++ include/ice/Utility.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace ice {
+	std::string Format(const std::string& format, const std::vector<std::string>& arguments);
+}
---
+++ src/Utility.cpp
@@ -0,0 +1,29 @@
+#include <ice/Utility.hpp>
+
+namespace ice {
+	std::string Format(const std::string& format, const std::vector<std::string>& arguments) {
+		std::string result;
+		result.reserve(format.size() + 64);
+
+		std::size_t begin = 0;
+		std::size_t percent;
+		std::size_t index = 0;
+
+		while ((percent = format.find('%', begin)) != std::string::npos) {
+			result.insert(result.end(), format.begin() + begin, format.begin() + percent);
+			if (percent + 1 < format.size() && format[percent + 1] == '%') {
+				result.push_back('%');
+				begin = percent + 2;
+			} else {
+				result.append(arguments[index++]);
+				begin = percent + 1;
+			}
+		}
+
+		if (begin < format.size()) {
+			result.insert(result.end(), format.begin() + begin, format.end());
+		}
+
+		return result;
+	}
+}
